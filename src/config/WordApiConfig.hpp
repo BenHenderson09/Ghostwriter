ca0d1b7e17d60e605fd19000262c89b1b974d96c
Ben Henderson <benh090704@gmail.com>
2020-02-09 21:55:40 +0000

    Replaced obsolete API key

--- src/config/WordApiConfig.hpp
+++ src/config/WordApiConfig.hpp
@@ -7,6 +7,6 @@ const std::string API_HOST_HEADER =
     "x-rapidapi-host: wordsapiv1.p.rapidapi.com";
         
 const std::string API_KEY_HEADER =
-    "x-rapidapi-key: 862ef75b92msh0ece2869b56cec7p1e324ajsn511089d3d4c7";
+    "x-rapidapi-key: <YOUR API KEY>";
 
 #endif
